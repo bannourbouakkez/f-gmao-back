25d689d500100dbd2fb89bfb593394c400c0d63d
Bannour <bouakkezbannour@gmail.com>
2021-04-11 15:54:39 +0200

    fillable na77itha jemla

--- app/Model/Achat/fournisseur.php
+++ app/Model/Achat/fournisseur.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class fournisseur extends Model
 {
-     protected $fillable =   ['livmode_id'];
+    //protected $fillable =   ['livmode_id'];
     protected $guarded = ['id','created_at,updated_at'];
     protected $primaryKey = 'id';
     
