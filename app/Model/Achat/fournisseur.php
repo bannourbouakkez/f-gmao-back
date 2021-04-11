452fab6801c67c4ec20e74b34b7d628f99284d44
Bannour <bouakkezbannour@gmail.com>
2021-04-11 16:01:18 +0200

    na77it el guarded

--- app/Model/Achat/fournisseur.php
+++ app/Model/Achat/fournisseur.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class fournisseur extends Model
 {
-    //protected $fillable =   ['livmode_id'];
-    protected $guarded = ['id','created_at,updated_at'];
+    protected $fillable =  [];
+    //protected $guarded = ['id','created_at,updated_at'];
     protected $primaryKey = 'id';
     
 }
