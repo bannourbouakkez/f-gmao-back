18fc82fa6faa7a69c4cb626fce011dc3a9f0ada8
Bannour <bouakkezbannour@gmail.com>
2021-04-11 16:15:00 +0200

    function setfillable ama na7it fillable w guarded

--- app/Model/Achat/fournisseur.php
+++ app/Model/Achat/fournisseur.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class fournisseur extends Model
 {
-    protected $fillable =  [];
-    protected $guarded = ['id','created_at','updated_at'];
+    //protected $fillable =  [];
+    //protected $guarded = ['id','created_at','updated_at'];
     protected $primaryKey = 'id';
     
 }
